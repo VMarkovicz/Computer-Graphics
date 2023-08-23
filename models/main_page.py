98a03472de5f33bb7d67d2a034028ccd0ce7d1cc
Guilherme Sanches <guitdsanches@gmail.com>
2023-08-23 12:58:13 -0300

    fix: folders

--- models/main_page.py
+++ models/main_page.py
@@ -1,10 +1,11 @@
 import sys
 from PyQt5 import QtGui
 from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox
-from PyQt5.QtGui import QPainter, QBrush, QPen, QMouseEvent
+from PyQt5.QtGui import QPainter, QBrush, QPen, QMouseEvent, QPolygon, QPolygonF
 from PyQt5.QtCore import Qt
-from design.design import *
 import pyautogui
+
+from design.design import *
 
 class MainPage(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
@@ -13,29 +14,54 @@ class MainPage(QMainWindow, Ui_MainWindow):
         self.scene = QtWidgets.QGraphicsScene()
         self.graphics.setScene(self.scene)
         self.setWindowButton.clicked.connect(self.paintEvent)
-        self.pen = QtGui.QPen(QtCore.Qt.green)
+        self.pen = QtGui.QPen(QtCore.Qt.green, 3)
+        self.brush = QtGui.QBrush(QtCore.Qt.red)
         self.initial_point = None
         self.final_point = None
+        self.point_button.clicked.connect(self.criar_ponto)
+        self.inicia_eixo()
+        self.pen = QtGui.QPen(QtCore.Qt.black,5)
+        self.line_button.clicked.connect(self.criar_linha)
+        self.triangle_button.clicked.connect(self.criar_triangulo)
+        
+    def inicia_eixo(self):
+        reta_x = QtCore.QLineF(QtCore.QPointF(-500,0), QtCore.QPointF(500,0))
+        self.scene.addLine(reta_x, self.pen)
+        reta_y = QtCore.QLineF(QtCore.QPointF(0,-500), QtCore.QPointF(0, 500))
+        self.scene.addLine(reta_y, self.pen)
+        
 
-        """ 
-        for i in range(16): 455 no x, 106 y
-            for j in range(16):
-                r = QtCore.QRectF(QtCore.QPointF(i*side, j*side), QtCore.QSizeF(side, side))
-                self.scene.addRect(r, self.pen) """
-
-    def mouseDoubleClickEvent(self, event): #point
-        r = QtCore.QRectF(event.pos(), QtCore.QSizeF(5, 5))
+    
+    def criar_ponto(self):
+        x = int(self.x_point.text())
+        y = int(self.y_point.text()) * -1
+        ponto = QtCore.QPointF(x, y)
+        r = QtCore.QRectF(ponto, QtCore.QSizeF(5,5))
         self.scene.addRect(r, self.pen)
 
-    """ def mousePressEvent(self, event): #line
-        self.initial_point = event.pos()
-        r = QtCore.QLineF(QtCore.QPointF(10,10),QtCore.QPointF(50,50))
-        self.scene.addLine(r,self.pen)
-    def mouseReleaseEvent(self, event):
-        self.final_point = event.pos()
-        r = QtCore.QLineF(self.initial_point,self.final_point)
-        self.scene.addLine(r,self.pen) """
-        
+    def criar_linha(self):
+        x1 = int(self.line_x1_point.text())
+        y1 = int(self.line_y1_point.text()) * -1
+        x2 = int(self.line_x2_point.text())
+        y2 = int(self.line_y2_point.text()) * -1
+        line = QtCore.QLineF(QtCore.QPointF(x1,y1), QtCore.QPointF(x2,y2))
+        self.scene.addLine(line, self.pen)
+
+    def criar_triangulo(self):
+        x1 = int(self.triangle_x1_point.text())
+        x2 = int(self.triangle_x2_point.text())
+        x3 = int(self.triangle_x3_point.text())
+        y1 = int(self.triangle_y1_point.text()) * -1
+        y2 = int(self.triangle_y2_point.text()) * -1
+        y3 = int(self.triangle_y3_point.text()) * -1
+        triangle = QtGui.QPolygonF()
+        triangle.append(QtCore.QPointF(x1,y1))
+        triangle.append(QtCore.QPointF(x2,y2))
+        triangle.append(QtCore.QPointF(x3,y3))
+        self.scene.addPolygon(triangle, self.pen,self.brush)
+    
+    def zoom_in(self):
+        print()
 
     
 
--- sgi_py/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from models.main_page import *
-from PyQt5.QtWidgets import QMainWindow, QApplication,QMessageBox
-
-if __name__ == "__main__":
-    qt = QApplication(sys.argv)
-    loadUi = MainPage() 
-    loadUi.show()
-    qt.exec_()
